51c603aabb9cc348442f9ae465ed46ed5821b464
George Lestaris <glestaris@gmail.com>
2016-09-08 17:02:47 +0100

    cc experiment handles full runC lifecycle

--- ice-experiment/cc.py
+++ ice-experiment/cc.py
@@ -1,6 +1,11 @@
+from os import path
 import fabric.api as fab
 import fabric.contrib.files as fab_files
 import ice
+
+
+BUNDLES_BASE_PATH = '/var/lib/containers'
+CVMFS_BASE_PATH = '/cvmfs/container-images.aws'
 
 ###############################################################################
 # Runners
@@ -27,6 +32,66 @@ def mount_cvmfs(hosts, server_fqdn, cert_path):
     with fab.hide('running'):
         fab.execute(write_cvmfs_config_task, hosts, server_fqdn, cert_path)
         res = fab.execute(mount_cvmfs_task, hosts)
+    _print_outcomes(res)
+
+
+@ice.ParallelRunner
+def runc_start(hosts, image, id, *args):
+    """Start a container using a CernVM-FS backed container image.
+
+    Args:
+        image: The CernVM-FS container image to use.
+        id: The runC container id.
+        args: Command to run in container.
+    """
+    with fab.hide('running'):
+        res = fab.execute(make_runc_bundle_task, hosts, image, id)
+    if not _check_outcomes(res):
+        print('ERROR: Failed to make runC bundles!')
+        _print_outcomes(res)
+        return False
+
+    with fab.hide('running'):
+        res = fab.execute(make_runc_config_task, hosts, id, args)
+    if not _check_outcomes(res):
+        print('ERROR: Failed to make runC config in bundles!')
+        _print_outcomes(res)
+        return False
+
+    with fab.hide('running'):
+        res = fab.execute(runc_start_task, hosts, id)
+    _print_outcomes(res)
+
+
+@ice.ParallelRunner
+def runc_exec(hosts, id, *args):
+    """Execute a process inside an existing container.
+
+    Args:
+        id: The runC container id.
+        args: Command to run in container.
+    """
+    with fab.hide('running'):
+        res = fab.execute(runc_exec_task, hosts, id, args)
+    _print_outcomes(res)
+
+
+@ice.ParallelRunner
+def runc_delete(hosts, id):
+    """Delete a running container.
+
+    Args:
+        id: The runC container id.
+    """
+    with fab.hide('running'):
+        res = fab.execute(runc_kill_task, hosts, id)
+    if not _check_outcomes(res):
+        print('ERROR: Failed to kill runC containers!')
+        _print_outcomes(res)
+        return False
+
+    with fab.hide('running'):
+        res = fab.execute(delete_runc_bundle_task, hosts, id)
     _print_outcomes(res)
 
 ###############################################################################
@@ -73,15 +138,114 @@ def mount_cvmfs_task(hosts):
         warn_only=True
     )
 
+
+def make_runc_bundle_task(hosts, image, id):
+    bundle_path = path.join(BUNDLES_BASE_PATH, id)
+    image_path = path.join(CVMFS_BASE_PATH, 'image', image)
+    rw_branch_path = path.join(bundle_path, 'rw')
+    rootfs_path = path.join(bundle_path, 'rootfs')
+
+    if not fab_files.exists(image_path, use_sudo=True):
+        return _error('image `{:s}` does not exist'.format(image))
+    if fab_files.exists(bundle_path, use_sudo=True):
+        return _error('`{:s}` exists'.format(bundle_path))
+
+    fab.sudo('mkdir -p {:s}'.format(bundle_path))
+    fab.sudo('mkdir {:s}'.format(rw_branch_path))
+    fab.sudo('mkdir {:s}'.format(rootfs_path))
+
+    return fab.sudo(
+        'mount -t aufs -o br={:s}=rw:{:s}=ro none {:s}'.format(
+            rw_branch_path, image_path, rootfs_path
+        ),
+        warn_only=True,
+    )
+
+
+def make_runc_config_task(hosts, id, args):
+    bundle_path = path.join(BUNDLES_BASE_PATH, id)
+    config_path = path.join(bundle_path, "config.json")
+
+    if not fab_files.exists(bundle_path, use_sudo=True):
+        return _error('`{:s}` does not exist'.format(bundle_path))
+
+    cmd = 'ocitools generate'
+    for arg in args:
+        cmd += ' --args {:s}'.format(arg)
+
+    return fab.sudo('{:s} > {:s}'.format(cmd, config_path), warn_only=True)
+
+
+def runc_start_task(hosts, id):
+    bundle_path = path.join(BUNDLES_BASE_PATH, id)
+
+    if not fab_files.exists(bundle_path, use_sudo=True):
+        return _error('`{:s}` does not exist'.format(bundle_path))
+
+    with fab.cd(bundle_path):
+        fab.sudo('runc create {:s}'.format(id))
+        return fab.sudo('runc start {:s}'.format(id), warn_only=True)
+
+
+def runc_exec_task(hosts, id, args):
+    bundle_path = path.join(BUNDLES_BASE_PATH, id)
+
+    if not fab_files.exists(bundle_path, use_sudo=True):
+        return _error('`{:s}` does not exist'.format(bundle_path))
+
+    with fab.cd(bundle_path):
+        return fab.sudo('runc exec {:s} {:s}'.format(
+            id, ' '.join(args)
+        ), warn_only=True)
+
+
+def runc_kill_task(hosts, id):
+    bundle_path = path.join(BUNDLES_BASE_PATH, id)
+
+    if not fab_files.exists(bundle_path, use_sudo=True):
+        return _error('`{:s}` does not exist'.format(bundle_path))
+
+    with fab.cd(bundle_path):
+        fab.sudo('runc kill {:s}'.format(id))
+        return fab.sudo('runc delete {:s}'.format(id), warn_only=True)
+
+
+def delete_runc_bundle_task(hosts, id):
+    bundle_path = path.join(BUNDLES_BASE_PATH, id)
+    rootfs_path = path.join(bundle_path, 'rootfs')
+
+    if not fab_files.exists(bundle_path, use_sudo=True):
+        return _error('`{:s}` does not exist'.format(bundle_path))
+
+    fab.sudo('umount {:s}'.format(rootfs_path))
+    return fab.sudo('rm -Rf {:s}'.format(bundle_path))
+
 ###############################################################################
 # Helpers
 ###############################################################################
 
 
-def _print_outcomes(res):
+class _error():
+    def __init__(self, msg):
+        self.failed = True
+        self.msg = msg
+        print('ERROR: {:s}'.format(msg))
+
+
+def _check_outcomes(res):
     for key, value in res.items():
         if value.failed:
-            outcome = '[FAIL]'
+            return False
+    return True
+
+
+def _print_outcomes(res):
+    for key, value in res.items():
+        if hasattr(value, 'failed') and value.failed:
+            if hasattr(value, 'msg'):
+                outcome = '[FAIL] - {:s}'.format(value.msg)
+            else:
+                outcome = '[FAIL]'
         elif value != '':
             outcome = '[OK] - {}'.format(value)
         else:
